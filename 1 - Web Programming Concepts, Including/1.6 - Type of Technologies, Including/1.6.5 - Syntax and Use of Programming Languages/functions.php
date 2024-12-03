21beff3837cce9808969a5892522bde771cd009b
Andhyvlz <Andhyvlz03@gmail.com>
2024-12-04 00:06:08 +1100

    update '1 - Web Programming Concepts, Including/1.6 - Type of Technologies, Including/1.6.5 - Syntax and Use of Programming Languages/index.php'

---
+++ 1 - Web Programming Concepts, Including/1.6 - Type of Technologies, Including/1.6.5 - Syntax and Use of Programming Languages/functions.php
@@ -0,0 +1,9 @@
+<?php
+    function navigation() {
+        echo '
+        <a href="index.php">Index</a>
+        <a href="page2.php">Page 2</a>
+        <a href="page3.php">Page 3</a>
+        ';
+    }
+?>
