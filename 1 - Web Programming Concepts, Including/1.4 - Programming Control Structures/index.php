b912ee5299553262f39be22bc6e1a12c18814858
Andhyvlz <Andhyvlz03@gmail.com>
2024-12-03 14:04:11 +1100

    index.php: show the structure of if statement

---
+++ 1 - Web Programming Concepts, Including/1.4 - Programming Control Structures/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <code>
+        if (this > that) { 
+            <br>
+            do cool stuff there
+        } else { 
+            <br>
+            information user that something has gone horribly wrong 
+            <br>
+        }
+    </code>
+</body>
+</html>
